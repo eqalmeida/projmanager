b9434b4c8729f674091eed0322c48bc3d036b060
Eduardo Almeida <eqalmeida@gmail.com>
2018-02-18 23:16:37 -0300

    Versao inicial

---
+++ projetos/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+
+from .models import Problem, Project, Pacote
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pacote', 'colored_type', 'priority', 'complete' )
+    list_filter = ('pacote__projeto', 'prob_type', 'priority','complete')
+    search_fields = ('title',)
+
+@admin.register(Pacote)
+class PacoteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'projeto', 'percent')
+    list_filter = ('projeto',)
+    fields = ('name', 'projeto', 'percent')
+    search_fields = ('name',)
+
+class PacoteInline(admin.TabularInline):
+    model = Pacote
+    extra = 1    
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('get_percent',)
+    list_display = ('name', 'get_percent')
+    inlines = [
+        PacoteInline,
+    ]
+
+    def get_percent(self, obj):
+        """
+        Calcula a média percentual do projeto
+        """
+        pacotes = Pacote.objects.filter(projeto=obj)
+        if len(pacotes) > 0:
+            soma = 0
+            for pacote in pacotes:
+                soma += pacote.percent
+
+            return int(soma/len(pacotes)) 
+        else:
+            return 0
+
+    get_percent.short_description = 'Percentual (%)'
+
---
+++ projetos/models.py
@@ -0,0 +1,88 @@
+
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.utils.html import format_html
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Projeto'
+        verbose_name_plural = 'Projetos'        
+
+class Pacote(models.Model):
+    projeto = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField('Nome', max_length=50)
+    percent = models.IntegerField('Percentual', default=0)
+
+    def __str__(self):
+        return "{} | {}".format(self.projeto.name, self.name)
+
+    class Meta:
+        unique_together = (('projeto','name'),)
+
+
+class Problem(models.Model):
+
+    PROB_TYPE = (
+        ('E', 'Erro'),
+        ('Q', 'Dúvida'),
+        ('S', 'Sugestão'),
+        ('P', 'Pendência'),
+    )
+
+    PRIORITIES = (
+        (0, 'Alta'),
+        (1, 'Média'),
+        (2, 'Baixa'),
+    )
+
+    pacote = models.ForeignKey( Pacote, on_delete=models.CASCADE, verbose_name='Pacote')
+    title = models.CharField('Título', max_length=200)
+    text = models.TextField('Descrição detalhada', blank=True)
+
+    prob_type = models.CharField(
+        'Tipo',
+        max_length=1,
+        choices=PROB_TYPE,
+        default=' '
+    )
+
+    priority = models.IntegerField(
+        'Prioridade',
+        choices=PRIORITIES,
+        default=1
+    )
+
+    versao = models.CharField('Versão', max_length=20, blank=True)
+
+    complete = models.BooleanField('Concluído?', default=False)
+
+    def colored_type(self):
+        
+        COLOR_TYPE = {
+            'E': 'FF3333',
+            'Q': '00AA00',
+            'S': '00AA00',
+            'P': '0000AA',
+        }
+        
+        return format_html(
+            '<span style="color: #{};">{}</span>',
+            COLOR_TYPE[self.prob_type],
+            self.get_prob_type_display(),
+        )
+
+    def __str__(self):
+        return "{}".format(self.title)
+
+    colored_type.short_description = 'Tipo'
+
+    class Meta:
+        verbose_name = 'Problema'
+        verbose_name_plural = 'Problemas'
+    
